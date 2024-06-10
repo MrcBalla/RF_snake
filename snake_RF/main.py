5c746bf63cc458fd6984956d15391830de44c9d7
MrcBalla <marco00.ballarini@gmail.com>
2024-06-10 18:53:06 +0200

    added and updataed main file for debug

--- snake_RF/main.py
+++ snake_RF/main.py
@@ -27,23 +27,30 @@ def main():
     reward_history_conv=[0]
     EPOCHS=1500
     env_ = get_env()
-    agent=TRPO_agent(env=env_)
+    agent=DQN(env=env_)
+    step=1
     for e in range(EPOCHS):
         if e%50==0:
             print(f"{e}/{EPOCHS} - {np.mean(reward_history_conv[-30:]) or 0}", end="\n")
-        state=env_.to_state()
-        #state=tf.reshape(state, (1000, -1))
+        
+        #if e%400==0 and e!=0:
+        #    step=step+1        
 
-        original_probs = agent.actor(state)
-        pred = original_probs / tf.reduce_sum(original_probs, axis=-1, keepdims=True)
-        samples = tf.random.categorical(tf.math.log(pred), 1, dtype=tf.int32)[:, 0]
+        state=env_.to_state()
+        state=tf.reshape(state, (1000, -1))
+
+        value_q=agent.Q_net(state)
+        #original_probs = agent.Q_net(state)
+        #pred = original_probs / tf.reduce_sum(original_probs, axis=-1, keepdims=True)
+        #samples = tf.random.categorical(tf.math.log(pred), 1, dtype=tf.int32)[:, 0]
+        samples=tf.argmax(value_q, axis=1)
         actions=tf.reshape(samples, (samples.shape[0],1))
 
         masks=env_.check_actions(actions)
 
         rewards= env_.move(actions)
         new_state = tf.constant(env_.to_state())
-        #new_state=tf.reshape(new_state, (1000, -1))
+        new_state=tf.reshape(new_state, (1000, -1))
 
         agent.learn(state, new_state, samples, rewards) # this optimie the policy function tarting from information sampled
         if e > 50: reward_history_conv.append(np.mean(rewards))
