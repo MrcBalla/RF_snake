0cdb71cda075cd90f1f62e681eee95c481da2fe8
MrcBalla <marco00.ballarini@gmail.com>
2024-08-25 15:40:30 +0200

    final changes

--- snake_RF/agent.py
+++ snake_RF/agent.py
@@ -6,7 +6,7 @@ from environments_partially_observable import *
 import environments_fully_observable 
 import math
     
-class new_agent:
+class PPO:
     def __init__(self, env:environments_fully_observable, step_size=0.001, batch_size=256) -> None:
         self.discount = 0.95
         self.clip_eps = 0
@@ -36,9 +36,9 @@ class new_agent:
         self.optimizer_critic = tf.optimizers.legacy.Adam(step_size)
         self.batch_size = batch_size
 
-    def print_model(self):
-        print(self.actor.summary())
-        print(self.critic.summary())
+    def save(self,path):
+        tf.keras.saving.save_model(self.actor, filepath=path+"PPO_actor.h5",save_format='h5')
+        tf.keras.saving.save_model(self.critic, filepath=path+"PPO_critic.h5",save_format='h5')
 
     def learn(self, states, new_states, samples, rewards):
         rewards=np.reshape(rewards, (-1,1)) # reshape of rewards
@@ -49,10 +49,6 @@ class new_agent:
         reward_to_go = tf.stop_gradient(rewards + self.discount * new_val) # stop gradient 
         td_error = (reward_to_go - val).numpy() # td error phase
         
-        # the td error is the unbiased estimator of the advantage function, imporatence sampling ratio is 
-        # represented by r_t in the formula and its used to keeps variance down and computed as the ratio
-        # between the selected action probs and the initial probs
-
         for _ in range(self.actor_rep):
             indexes=np.random.choice(range(0, len(states)), min(self.batch_size, len(states)), replace=False)
             with tf.GradientTape() as a_tape:
@@ -83,20 +79,20 @@ class new_agent:
             self.optimizer_critic.apply_gradients(zip(grad_critic, self.critic.trainable_weights))
         
 
-class conv_agent:
+class PPO_conv:
     def __init__(self, env:environments_fully_observable, step_size=0.001, batch_size=256) -> None:
         self.discount = 0.95
         self.clip_eps = 0
         self.actor_rep = 20
         self.critic_rep = 5
         self.actor = tf.keras.Sequential([
-            tf.keras.layers.Conv2D(32, kernel_size=(3,3), padding='same'),
+            tf.keras.layers.Conv2D(32, kernel_size=(2,2), padding='same'),
             tf.keras.layers.BatchNormalization(),
             tf.keras.layers.Activation(tf.nn.tanh),
-            tf.keras.layers.Conv2D(16, kernel_size=(3,3)),
+            tf.keras.layers.Conv2D(16, kernel_size=(2,2)),
             tf.keras.layers.BatchNormalization(),
             tf.keras.layers.Activation(tf.nn.tanh),
-            tf.keras.layers.Conv2D(5, kernel_size=(2,2)),
+            tf.keras.layers.Conv2D(5, kernel_size=(3,3)),
             tf.keras.layers.BatchNormalization(),
             tf.keras.layers.Activation(tf.nn.tanh),
             tf.keras.layers.Flatten(),
@@ -105,13 +101,13 @@ class conv_agent:
                                   bias_initializer=tf.initializers.RandomNormal(stddev=0.005))
         ])
         self.critic = tf.keras.Sequential([
-            tf.keras.layers.Conv2D(32, kernel_size=(5,5), padding='same'),
+            tf.keras.layers.Conv2D(32, kernel_size=(2,2), padding='same'),
             tf.keras.layers.BatchNormalization(),
             tf.keras.layers.Activation(tf.nn.tanh),
             tf.keras.layers.Conv2D(16, kernel_size=(3,3)),
             tf.keras.layers.BatchNormalization(),
             tf.keras.layers.Activation(tf.nn.tanh),
-            tf.keras.layers.Conv2D(5, kernel_size=(2,2)),
+            tf.keras.layers.Conv2D(5, kernel_size=(5,5)),
             tf.keras.layers.BatchNormalization(),
             tf.keras.layers.Activation(tf.nn.tanh),
             tf.keras.layers.Flatten(),
@@ -122,9 +118,10 @@ class conv_agent:
         self.optimizer_critic = tf.optimizers.legacy.Adam(step_size)
         self.batch_size = batch_size
 
-    def print_model(self):
-        print(self.actor.summary())
-        print(self.critic.summary())
+    def save(self,path):
+        tf.keras.saving.save_model(self.actor, filepath=path+"PPO_actor_conv.h5",save_format='h5')
+        tf.keras.saving.save_model(self.critic, filepath=path+"PPO_critic_conv.h5",save_format='h5')
+
 
     def learn(self, states, new_states, samples, rewards):
         rewards=np.reshape(rewards, (-1,1)) # reshape of rewards
@@ -135,10 +132,6 @@ class conv_agent:
         reward_to_go = tf.stop_gradient(rewards + self.discount * new_val) # stop gradient 
         td_error = (reward_to_go - val).numpy() # td error phase
         
-        # the td error is the unbiased estimator of the advantage function, imporatence sampling ratio is 
-        # represented by r_t in the formula and its used to keeps variance down and computed as the ratio
-        # between the selected action probs and the initial probs
-
         for _ in range(self.actor_rep):
             indexes=np.random.choice(range(0, len(states)), min(self.batch_size, len(states)), replace=False)
             with tf.GradientTape() as a_tape:
@@ -168,94 +161,6 @@ class conv_agent:
             grad_critic = c_tape.gradient(loss_critic, self.critic.trainable_weights)
             self.optimizer_critic.apply_gradients(zip(grad_critic, self.critic.trainable_weights))
         
-
-class TRPO_agent:
-    def __init__(self, env:environments_fully_observable, step_size=0.001, batch_size=256) -> None:
-        self.discount = 0.95
-        self.clip_eps = 0
-        self.actor_rep = 20
-        self.critic_rep = 5
-        self.actor = tf.keras.Sequential([
-            tf.keras.layers.Conv2D(32, kernel_size=(3,3), padding='same'),
-            tf.keras.layers.BatchNormalization(),
-            tf.keras.layers.Activation(tf.nn.tanh),
-            tf.keras.layers.Conv2D(16, kernel_size=(3,3)),
-            tf.keras.layers.BatchNormalization(),
-            tf.keras.layers.Activation(tf.nn.tanh),
-            tf.keras.layers.Conv2D(5, kernel_size=(2,2)),
-            tf.keras.layers.BatchNormalization(),
-            tf.keras.layers.Activation(tf.nn.tanh),
-            tf.keras.layers.Flatten(),
-            tf.keras.layers.Dense(5, activation=tf.nn.softmax,
-                                  kernel_initializer=tf.initializers.RandomNormal(stddev=0.005),
-                                  bias_initializer=tf.initializers.RandomNormal(stddev=0.005))
-        ])
-        self.critic = tf.keras.Sequential([
-            tf.keras.layers.Conv2D(32, kernel_size=(5,5), padding='same'),
-            tf.keras.layers.BatchNormalization(),
-            tf.keras.layers.Activation(tf.nn.tanh),
-            tf.keras.layers.Conv2D(16, kernel_size=(3,3)),
-            tf.keras.layers.BatchNormalization(),
-            tf.keras.layers.Activation(tf.nn.tanh),
-            tf.keras.layers.Conv2D(5, kernel_size=(2,2)),
-            tf.keras.layers.BatchNormalization(),
-            tf.keras.layers.Activation(tf.nn.tanh),
-            tf.keras.layers.Flatten(),
-            tf.keras.layers.Dense(1, kernel_initializer=tf.initializers.RandomNormal(stddev=0.005),
-                                  bias_initializer=tf.initializers.RandomNormal(stddev=0.005))
-        ])
-        self.optimizer_actor = tf.optimizers.legacy.Adam(step_size)
-        self.optimizer_critic = tf.optimizers.legacy.Adam(step_size)
-        self.batch_size = batch_size
-
-    def KL_div(self, p_1, p_2):
-        kl = tf.reduce_sum(p_1 * (tf.math.log(p_1 + 1e-8) - tf.math.log(p_2 + 1e-8)), axis=-1)
-        return kl
-    
-    def learn(self, states, new_states, samples, rewards ,step):
-        rewards=np.reshape(rewards, (-1,1)) # reshape of rewards
-        val = self.critic(states) # compute values for creitic, so value function approximation
-        actions = tf.one_hot(samples, depth=5) # create one hot encoding vector for action
-        initial_probs = None
-        new_val = self.critic(new_states) # create value function at new state 
-        reward_to_go = tf.stop_gradient(rewards + self.discount * new_val) # stop gradient 
-        td_error = (reward_to_go - val).numpy() # td error phase
-        t=1
-        
-        for _ in range(self.actor_rep):
-            indexes=np.random.choice(range(0, len(states)), min(self.batch_size, len(states)), replace=False)
-            with tf.GradientTape() as a_tape:
-                probs = self.actor(tf.gather(states, indexes))
-                probs = probs / tf.reduce_sum(probs, axis=-1, keepdims=True)
-                selected_actions_probs = tf.reduce_sum(probs * np.array(tf.gather(actions, indexes)), axis=-1, keepdims=True) 
-                if initial_probs is None: 
-                    initial_probs = tf.convert_to_tensor(tf.stop_gradient(selected_actions_probs))
-                importance_sampling_ratio = selected_actions_probs / initial_probs
-                
-                kl_div=tf.reshape(((0.25/step)*self.KL_div(initial_probs,selected_actions_probs)), (256,1))
-                loss_actor = (td_error[indexes]*importance_sampling_ratio-kl_div)
-                
-                loss_actor = tf.reduce_mean(-loss_actor)
-                t=t+1
-
-            grad_actor = a_tape.gradient(loss_actor, self.actor.trainable_weights)
-            self.optimizer_actor.apply_gradients(zip(grad_actor, self.actor.trainable_weights))
-
-
-        for _ in range(self.critic_rep):
-            indexes = np.random.choice(range(0, len(states)), min(self.batch_size, len(states)), replace=False)
-            with tf.GradientTape() as c_tape:
-                val = self.critic(tf.gather(states, indexes))
-                new_val = tf.stop_gradient(self.critic(tf.gather(new_states, indexes)))
-                reward_to_go = tf.stop_gradient(rewards[indexes] + self.discount * new_val)
-                loss_critic = tf.losses.mean_squared_error(val, reward_to_go)[:, None]
-                loss_critic = tf.reduce_mean(loss_critic)
-                #print("loss critic:{}".format(loss_critic))
-
-            grad_critic = c_tape.gradient(loss_critic, self.critic.trainable_weights)
-            self.optimizer_critic.apply_gradients(zip(grad_critic, self.critic.trainable_weights))
-
-
 class DQN:
     def __init__(self, env:environments_fully_observable, step_size=0.001, batch_size=256) -> None:
         self.discount = 0.95
@@ -274,6 +179,10 @@ class DQN:
     
         self.Q_net_optim = tf.optimizers.legacy.Adam(step_size)
         self.batch_size = batch_size
+    
+    def save(self,path):
+        tf.keras.saving.save_model(self.Q_net, filepath=path+"Qnet.h5",save_format='h5')
+
 
     def learn(self, states, new_states, actions, rewards):
         rewards=np.reshape(rewards, (-1,1)) # reshape of rewards
@@ -282,8 +191,7 @@ class DQN:
 
         initial_probs=None
 
-        #print("start  ---------------------------------------------")
-        for _ in range(10):
+        for _ in range(20):
             indexes = np.random.choice(range(0, len(states)), min(self.batch_size, len(states)), replace=False)
             with tf.GradientTape() as c_tape:
                 actions_index=tf.gather(actions, indexes)
@@ -295,13 +203,60 @@ class DQN:
                 
                 loss = tf.losses.mean_squared_error(y, y_hat)[:, None]
                 loss = tf.reduce_mean(loss)
-                #print(loss)
+        
+            
+            grad_q_net = c_tape.gradient(loss, self.Q_net.trainable_weights)
+            self.Q_net_optim.apply_gradients(zip(grad_q_net, self.Q_net.trainable_weights))
+
+class DQN_conv:
+    def __init__(self, env:environments_fully_observable, step_size=0.001, batch_size=256) -> None:
+        self.discount = 0.95
+        self.clip_eps = 0
+        self.Q_net = tf.keras.Sequential([
+            tf.keras.layers.Conv2D(32, kernel_size=(2,2), padding='same'),
+            tf.keras.layers.BatchNormalization(),
+            tf.keras.layers.Activation(tf.nn.tanh),
+            tf.keras.layers.Conv2D(16, kernel_size=(3,3)),
+            tf.keras.layers.BatchNormalization(),
+            tf.keras.layers.Activation(tf.nn.tanh),
+            tf.keras.layers.Conv2D(5, kernel_size=(5,5)),
+            tf.keras.layers.BatchNormalization(),
+            tf.keras.layers.Activation(tf.nn.tanh),
+            tf.keras.layers.Flatten(),
+            tf.keras.layers.Dense(1, kernel_initializer=tf.initializers.RandomNormal(stddev=0.005),
+                                  bias_initializer=tf.initializers.RandomNormal(stddev=0.005))
+        ])
+    
+        self.Q_net_optim = tf.optimizers.legacy.Adam(step_size)
+        self.batch_size = batch_size
+    
+    def save(self,path):
+        tf.keras.saving.save_model(self.Q_net, filepath=path+"Qnet_conv.h5",save_format='h5')
+
+    def learn(self, states, new_states, actions, rewards):
+        rewards=np.reshape(rewards, (-1,1)) # reshape of rewards
+        actions=tf.one_hot(actions, depth=5, dtype=tf.int32) 
+        actions=tf.cast(actions, tf.bool)
+
+        initial_probs=None
+
+        for _ in range(10):
+            indexes = np.random.choice(range(0, len(states)), min(self.batch_size, len(states)), replace=False)
+            with tf.GradientTape() as c_tape:
+                actions_index=tf.gather(actions, indexes)
+
+                q_v=tf.stop_gradient(self.Q_net(tf.gather(new_states, indexes)))
+                y=tf.stop_gradient(rewards[indexes]+self.discount*(tf.reduce_max(q_v, axis=1)))
+
+                y_hat=(tf.reshape(tf.boolean_mask(self.Q_net(tf.gather(states, indexes)), actions_index), (256,1)))
+                
+                loss = tf.losses.mean_squared_error(y, y_hat)[:, None]
+                loss = tf.reduce_mean(loss)
         
             
             grad_q_net = c_tape.gradient(loss, self.Q_net.trainable_weights)
             self.Q_net_optim.apply_gradients(zip(grad_q_net, self.Q_net.trainable_weights))
         
-        #print("end  ---------------------------------------------")
 
 def compute_neighbours(pos):
     pos_return_0 = np.copy(pos)
--- snake_RF/main.py
+++ snake_RF/main.py
@@ -24,86 +24,31 @@ def get_env(n=1000):
     return e
 
 def main():
-    reward_history_conv=[0]
-    EPOCHS=1500
+    reward_history_dqn=[0]
+    EPOCHS=1000
     env_ = get_env()
-    agent=DQN(env=env_)
-    step=1
+    agent=DQN_conv(env=env_)
     for e in range(EPOCHS):
-        
-        if e%50==0:
-            print(f"{e}/{EPOCHS} - {np.mean(reward_history_conv[-30:]) or 0}", end="\n")
-        
-        #if e%400==0 and e!=0:
-        #    step=step+1        
+            if e%50==0:
+                print(f"{e}/{EPOCHS} - {np.mean(reward_history_dqn[-30:]) or 0}", end="\n")
+            
+            state=env_.to_state()
+            #state=tf.reshape(state, (1000, -1))
 
-        state=env_.to_state()
-        state=tf.reshape(state, (1000, -1))
+            value_q=agent.Q_net(state)
+            samples=tf.argmax(value_q, axis=1)
+            actions=tf.reshape(samples, (samples.shape[0],1))
 
-        value_q=agent.Q_net(state)
-        #original_probs = agent.Q_net(state)
-        #pred = original_probs / tf.reduce_sum(original_probs, axis=-1, keepdims=True)
-        #samples = tf.random.categorical(tf.math.log(pred), 1, dtype=tf.int32)[:, 0]
-        samples=tf.argmax(value_q, axis=1)
-        actions=tf.reshape(samples, (samples.shape[0],1))
+            masks=env_.check_actions(actions)
 
-        masks=env_.check_actions(actions)
+            rewards= env_.move(actions)
+            new_state = tf.constant(env_.to_state())
+            #new_state=tf.reshape(new_state, (1000, -1))
 
-        rewards= env_.move(actions)
-        new_state = tf.constant(env_.to_state())
-        new_state=tf.reshape(new_state, (1000, -1))
-
-        agent.learn(state, new_state, samples, rewards) # this optimie the policy function tarting from information sampled
-        if e > 50: reward_history_conv.append(np.mean(rewards))
-    plt.plot(reward_history_conv)
-
-def rebuild_action(list_of_position):
-    action=[]
-    list_of_position=np.array(list_of_position)
-    for index in range(len(list_of_position)-1):
-        if np.all(list_of_position[index]-list_of_position[index+1]==np.array([0,1])):
-            action.append(3)
-        if np.all(list_of_position[index]-list_of_position[index+1]==np.array([1,0])):
-            action.append(0)
-        if np.all(list_of_position[index]-list_of_position[index+1]==np.array([0,-1])):
-            action.append(1)
-        if np.all(list_of_position[index]-list_of_position[index+1]==np.array([-1,0])):
-            action.append(2)
-    return action
-
-
-def rebuild_action_path(prioirty_stack, last_postion, lista: list):
-    if prioirty_stack[tuple(last_postion)]==None:
-        return lista
-    else:
-        prev=prioirty_stack[tuple(last_postion)]
-        lista.append(prev)
-        return rebuild_action_path(prioirty_stack, prev, lista)
-
-def main_bfs():
-    env_ = get_env()
-    state=env_.to_state()
-    action_list_all_state={}
-    for i in range(state.shape[0]):
-        p,sl=BFS_search(state[i,:,:])
-        action=rebuild_action_path(sl, p, [])
-        action.insert(0, p)
-        action_list=rebuild_action(action)
-        action_list_all_state[i]=action_list
-
-    #tf.reshape(tf.convert_to_tensor(action_list_all_state), (3341,1))
-
-    action=[]
-    for chiave in action_list_all_state.keys():
-        try:
-            action.append(action_list_all_state[chiave][0])
-        except TypeError:
-            action.append(action_list_all_state[chiave])
-    
-    reward=env_.move(tf.reshape(tf.convert_to_tensor(action), (1000,1)))
-        
-    print(1)
+            agent.learn(state, new_state, samples, rewards) # this optimie the policy function tarting from information sampled
+            if e > 50: reward_history_dqn.append(np.mean(rewards))
+    plt.plot(reward_history_dqn)
 
 
 if __name__=='__main__':
-    main_bfs()
+    main()
